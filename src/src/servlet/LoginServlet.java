6ccd5ce17d8a80379d55556bcf2af71d828ab47a
dojo2022-ShimizuYotaro <103239417+dojo2022-ShimizuYotaro@users.noreply.github.com>
2022-06-10 13:30:32 +0900

    shimizu loginserv

---
+++ src/src/servlet/LoginServlet.java
@@ -0,0 +1,13 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
