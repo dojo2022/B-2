32682fae27567f6607fd5f5a9550fd917969d41e
dojo2022-kawakami <kawakami-yuzu-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-14 18:11:11 +0900

    kawakami

--- src/src/servlet/CertificationServlet.java
+++ src/src/servlet/CertificationServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class CertificationServlet
@@ -22,29 +21,24 @@ public class CertificationServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+/*		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
 		if (session.getAttribute("id") == null) {
 			response.sendRedirect("/tasuma/LoginServlet");
 			return;
 		}
-
+*/
 		// My資格ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/certification.jsp");
 				dispatcher.forward(request, response);
 
 
 		// リクエストパラメータを取得する
-		// ユーザ名をスコープから取得→取得したユーザ名をもとに各種データをデータベースから取得＋スコープに保存
-		// 登録した資格のデータを持ってくる？
+		// ユーザ名と登録した資格を持ってきて表示する？
 		request.setCharacterEncoding("UTF-8");
 		String username = request.getParameter("USERNAME");
 		String certification = request.getParameter("CERTIFICATION");
 
-	/*	// セッションスコープにIDを格納する コメントアウトを解除するとうまくログイン画面が出てこない
-		HttpSession session = request.getSession();
-		session.setAttribute("username", new LoginUser(username));
-	*/
    }
 
 }
