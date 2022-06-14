0507bc0754f89aa04a97356e8432a3ec199f3cf0
dojo2022-kosaka <103231448+dojo2022-kosaka@users.noreply.github.com>
2022-06-14 17:57:47 +0900

    22/06/14_kosaka

---
+++ src/src/dao/Target_understandsDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+
+import model.Targets;
+
+public class Target_understandsDAO {
+
+}
---
+++ src/src/model/Target_understands.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.io.Serializable;
+
+public class Target_understands implements Serializable {
+	//フィールド名
+
+
+	//コンストラクタ（引数あり）
+
+	//コンストラクタ（引数なし）
+}
