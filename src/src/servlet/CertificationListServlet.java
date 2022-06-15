6265c423746ef5471431802eee81d19556f777ac
dojo2022-Takeda <103238150+dojo2022-Takeda@users.noreply.github.com>
2022-06-15 13:59:30 +0900

    エラー消し

--- src/src/servlet/CertificationListServlet.java
+++ src/src/servlet/CertificationListServlet.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,10 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dao.CertificationsDAO;
-import model.Certifications;
 
 /* DAOが出来上がり次第起動できるのかも */
 
@@ -43,49 +40,49 @@ import model.Certifications;
 
 			// 検索処理を行う
 			CertificationsDAO cDao = new CertificationsDAO();
-			List<Certifications> cardList = cDao.select(new CertificationsDAO(id, null, null, null))
+	//		List<Certifications> cardList = cDao.select(new CertificationsDAO(id, null, null, null))
 
 		    // 検索結果をリクエストスコープに格納する
-			request.setAttribute("cardList", cardList);
+	//		request.setAttribute("cardList", cardList);
 
 			// 結果ページにフォワードする
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/schedule.jsp");
-			dispatcher.forward(request.response);
+	//		dispatcher.forward(request.response);
 
 			} // 資格名の情報を持ちながら遷移する処理はここまで↑↑↑
 
 
 			// カテゴリ検索の処理↓↓↓
 			// リクエストパラメータを取得する
-			request.setCharacterEncoding("UTF-8"); {
-			String allit = request.getParameter("allit");
-			String gengo = request.getParameter("gengo");
-			String data = request.getParameter("data");
-			String sec = request.getParameter("sec");
-			String net = request.getParameter("net");
-			String mana = request.getParameter("mana");
-			String jimu = request.getParameter("jimu");
-			String des = request.getParameter("des");
+	//		request.setCharacterEncoding("UTF-8"); {
+	//		String allit = request.getParameter("allit");
+	//		String gengo = request.getParameter("gengo");
+	//		String data = request.getParameter("data");
+	//		String sec = request.getParameter("sec");
+	//		String net = request.getParameter("net");
+	//		String mana = request.getParameter("mana");
+	//		String jimu = request.getParameter("jimu");
+	//		String des = request.getParameter("des");
 
 			// 検索処理を行う
 			CertificationsDAO cDao = new CertificationsDAO();
-			List<Certifications> cardList = cDao.select(new Certifications(allit, gengo, data, sec, net, mana, jimu, des));
+	//		List<Certifications> cardList = cDao.select(new Certifications(allit, gengo, data, sec, net, mana, jimu, des));
 
 			// 検索結果をリクエストスコープに格納する
-			request.setAttribute("cardList", cardList);
+	//		request.setAttribute("cardList", cardList);
 
 			// 結果ページにフォワードする
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/certification_list.jsp");
-			dispatcher.forward(request, response);
+	//		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/certification_list.jsp");
+	//		dispatcher.forward(request, response);
 
 			// カテゴリ検索の処理はここまで↑↑↑
 			}
 
 
-			// 資格登録一覧ページにフォワードする
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Certification_List.jsp");
-			dispatcher.forward(request, response);
-		}
+	//		// 資格登録一覧ページにフォワードする
+	//		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Certification_List.jsp");
+	//		dispatcher.forward(request, response);
+	//	}
 
 
 		/**
