29f4a6616dd45243721f4c54e3eb5ac964b52777
dojo2022-kosaka <103231448+dojo2022-kosaka@users.noreply.github.com>
2022-06-21 17:13:38 +0900

    Update Understands.java

--- src/src/model/Understands.java
+++ src/src/model/Understands.java
@@ -6,16 +6,18 @@ import java.util.List;
 public class Understands implements Serializable {
 	private String certification;
 	private List<Target_understands> tuList;
+	private List<Today_targets> ttList;
 
 	//コンストラクタ(引数あり)
-	public Understands(String certification, List<Target_understands> tuList) {
+	public Understands(String certification, List<Target_understands> tuList, List<Today_targets> ttList) {
 		this.setCertification(certification);
 		this.setTuList(tuList);
+		this.setTtList(ttList);
 	}
 
 	//コンストラクタ(引数なし)
 	public Understands() {
-		this(null, null);
+		this(null, null, null);
 	}
 
 	public String getCertification() {
@@ -32,5 +34,13 @@ public class Understands implements Serializable {
 
 	public void setTuList(List<Target_understands> tuList) {
 		this.tuList = tuList;
+	}
+
+	public List<Today_targets> getTtList() {
+		return ttList;
+	}
+
+	public void setTtList(List<Today_targets> ttList) {
+		this.ttList = ttList;
 	}
 }
