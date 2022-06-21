bde2e55e1747dc94fcd07076169177b4f33dfe8f
dojo2022-kosaka <103231448+dojo2022-kosaka@users.noreply.github.com>
2022-06-21 13:50:28 +0900

    Update Understands.java

--- src/src/model/Understands.java
+++ src/src/model/Understands.java
@@ -1,7 +1,36 @@
 package model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Understands implements Serializable {
+	private String certification;
+	private List<Target_understands> tuList;
 
+	//コンストラクタ(引数あり)
+	public Understands(String certification, List<Target_understands> tuList) {
+		this.setCertification(certification);
+		this.setTuList(tuList);
+	}
+
+	//コンストラクタ(引数なし)
+	public Understands() {
+		this(null, null);
+	}
+
+	public String getCertification() {
+		return certification;
+	}
+
+	public void setCertification(String certification) {
+		this.certification = certification;
+	}
+
+	public List<Target_understands> getTuList() {
+		return tuList;
+	}
+
+	public void setTuList(List<Target_understands> tuList) {
+		this.tuList = tuList;
+	}
 }
