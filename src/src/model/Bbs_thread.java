9255429377701623d668241d60e6df20ddc20dbb
dojo2022-Takeda <103238150+dojo2022-Takeda@users.noreply.github.com>
2022-06-23 13:19:34 +0900

    Update Bbs_thread.java

--- src/src/model/Bbs_thread.java
+++ src/src/model/Bbs_thread.java
@@ -1,5 +1,39 @@
 package model;
 
-public class Bbs_thread {
+import java.io.Serializable;
+import java.util.List;
+
+public class Bbs_thread implements Serializable {
+	private String thread_bbs; // thread_bbs
+	private List<String> comment_bbs; // List
+
+	//引数ありコンストラクタ
+	public Bbs_thread(String thread_bbs, String comment_bbs) {
+		this.thread_bbs = thread_bbs;
+		this.comment_bbs = comment_bbs;
+	}
+
+// 	引数なしコンストラクタ
+	public Bbs_thread() {
+		this.threads_bbs = "";
+		this.comment_bbs = "";
+
+
+	// ゲッタとセッタ
+	public String getthread_bbs() {
+		return thread_bbs;
+	}
+
+	public void setthread_bbs(String thread_bbs) {
+		this.thread_bbs = thread_bbs;
+	}
+
+	public String getcomment_bbs() {
+		return comment_bbs;
+	}
+
+	public void setcomment_bbs(String comment_bbs) {
+		this.comment_bbs = comment_bbs;
+	}
 
 }
