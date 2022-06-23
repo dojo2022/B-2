c60b98f01938c902fe6d1c44c8b6f8db1a98da08
dojo2022-sakuma <103239248+dojo2022-sakuma@users.noreply.github.com>
2022-06-23 16:57:25 +0900

    sakuma

--- src/src/servlet/BbsServlet.java
+++ src/src/servlet/BbsServlet.java
@@ -13,7 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.CommentsDAO;
 import dao.ThreadsDAO;
+import model.Bbs_thread;
+import model.Comments;
 import model.LoginUser;
 import model.Threads;
 
@@ -38,7 +41,7 @@ public class BbsServlet extends HttpServlet {
 		String username = loginuser.getUsername();
 		// 登録済みの全スレッド検索
 		ThreadsDAO tDao = new ThreadsDAO();
-		List<Threads> threadList = tDao.select_threads(new Threads(null));
+		List<Threads> threadList = tDao.select_threads(null);
 
 		// 検索結果をリクエストスコープに格納する ☆カテゴリ、難易度を踏まえて資格名、カテゴリ、難易度を格納
 		request.setAttribute("threadList", threadList);
@@ -57,18 +60,30 @@ public class BbsServlet extends HttpServlet {
 		// カテゴリー検索
 		// リクエストパラメータを取得する ☆カテゴリ
 		request.setCharacterEncoding("UTF-8");
-		String category_bbs = request.getParameter("select_bbsCategory"); //カテゴリ
-		ThreadsDAO tDao = new ThreadsDAO();
-		List<Threads> threadList = tDao.select_threads(new Threads(category_bbs));
+		String submit = request.getParameter("submit"); //submitの値を取ってくる
+		if(submit.equals("検索")) {
+			String category_bbs = request.getParameter("select_bbsCategory");
+			ThreadsDAO tDao = new ThreadsDAO();
+			List<Threads> threadList = tDao.select_threads(category_bbs);
 
-		// 検索結果をリクエストスコープに格納する ☆カテゴリ、難易度を踏まえて資格名、カテゴリ、難易度を格納
-		request.setAttribute("threadList", threadList);
+			// List<Threads>検索結果をリクエストスコープに格納する ☆カテゴリ、難易度を踏まえて資格名、カテゴリ、難易度を格納
+			request.setAttribute("threadList", threadList);
 
-		// jspへ結果を返す
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/bbs.jsp");
-		dispatcher.forward(request, response);
-//		スレタイを押されたらタイトル、コメント、時間を持ってきてスコープに入れる
-
-	}
-
-}
+			// jspへ結果を返す
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/bbs.jsp");
+			dispatcher.forward(request, response);
+		}else {
+	//		スレタイを押されたらid,タイトルを持ってきてスコープに入れる
+			String thread_bbs = request.getParameter("submit");
+			ThreadsDAO thDao = new ThreadsDAO();
+			String threadTitle = thDao.to_thread(thread_bbs);
+			String threadID = thDao.getThread_id(thread_bbs);
+			CommentsDAO cDAO=new CommentsDAO();
+			List<Comments> commentlist= cDAO.to_thread(threadID);
+			HttpSession session = request.getSession();
+			session.setAttribute("comments",new Bbs_thread(threadTitle,commentlist));
+			// 結果を返す
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/bbs_thread.jsp");
+			dispatcher.forward(request, response);
+		}
+	}}
