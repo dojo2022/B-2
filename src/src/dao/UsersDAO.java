77d3dc6e26b5029feb7107d19aae5b282e025e0d
dojo2022-sakuma <103239248+dojo2022-sakuma@users.noreply.github.com>
2022-06-24 09:51:16 +0900

    Update UsersDAO.java

--- src/src/dao/UsersDAO.java
+++ src/src/dao/UsersDAO.java
@@ -1,5 +1,4 @@
 package dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -11,8 +10,6 @@ import java.util.List;
 import javax.servlet.annotation.WebServlet;
 
 import model.Users;
-
-
 /**
  * Servlet implementation class UsersDAO
  */
@@ -22,11 +19,9 @@ public class UsersDAO  {
 	public boolean isLoginOK(Users Users) {
 		Connection conn = null;
 		boolean loginResult = false;
-
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
-
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
 			// SELECT文を準備する
@@ -34,11 +29,9 @@ public class UsersDAO  {
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			pStmt.setString(1, Users.getUsername());
 			pStmt.setString(2,Users.getPassword());
-
 			// SELECT文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
 			//テーブルのような形で受け取る
-
 			// ユーザーIDとパスワードが一致するユーザーがいたかどうかをチェックする
 			rs.next();
 			//テーブルの一行目(count(*))に焦点を当てる
@@ -68,28 +61,23 @@ public class UsersDAO  {
 				}
 			}
 		}
-
 		// 結果を返す
 		return loginResult;
 	}
-
 	// 引数regisyで指定されたレコードを登録し、成功したらtrueを返す
 	public boolean insert(Users regist) {
 		Connection conn = null;
 		boolean result = false;
-
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
-
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
 			// SQL文を準備する
-			String sql = "insert into Users (username, password,mail) values (?,?,?)";
+			String sql = "insert into Users (username, password,mail) values (?,?,?);";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-
 			if (regist.getUsername() != null && !regist.getUsername().equals("")) {
 				pStmt.setString(1,regist.getUsername());
 			}
@@ -108,6 +96,45 @@ public class UsersDAO  {
 			else {
 				pStmt.setString(3, null);
 			}
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// 結果を返す
+		return result;
+	}
+
+	public boolean insert_update() {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+			// SQL文を準備する
+			String sql = "UPDATE Users SET  user_id=(SELECT CONCAT('u',id) FROM Users WHERE user_id IS NULL) WHERE user_id IS NULL";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
@@ -138,27 +165,20 @@ public class UsersDAO  {
 
 
 
-
-
 //ユーザ設定で使用するDAO
-
 	//old_usernameからold_mail,old_pw(ログインユーザーのメールアドレスとパスワード)を取得する
 	//ユーザ設定で入力なしの場合に現状のものを再設定するため
 	 public List<Users> select(Users param) {
 		Connection conn = null;
 		List<Users> cardList = new ArrayList<Users>();
-
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
-
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
 			// SQL文を準備する
 			String sql = "SELECT address, password from Users WHERE username LIKE ? ";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-
 			// SQL文を完成させる
 			if (param.getOld_username() != null) {
 				pStmt.setString(1,   param.getOld_username() );
@@ -166,10 +186,8 @@ public class UsersDAO  {
 			else {
 				pStmt.setString(1, "%" + param.getOld_username() + "%");
 			}
-
 			// SQL文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
-
 			// 結果表をコレクションにコピーする
 			while (rs.next()) {
 				Users card = new Users(
@@ -199,13 +217,9 @@ public class UsersDAO  {
 				}
 			}
 		}
-
 		// 結果を返す
 		return cardList;
 	}
-
-
-
 	// 引数Usersで指定されたレコードを更新し、成功したらtrueを返す
 		public boolean update(Users users) {
 			Connection conn = null;
@@ -213,32 +227,29 @@ public class UsersDAO  {
 			try {
 				// JDBCドライバを読み込む
 				Class.forName("org.h2.Driver");
-
 				// データベースに接続する
 				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
 				// SQL文を準備する
 				String sql = "update Users set USERNAME=?, PASSWORD=?, MAIL=?  where USERNAME=?";
 				PreparedStatement pStmt = conn.prepareStatement(sql);
-
 				// SQL文を完成させる
 				if (users.getUsername() != null && !users.getUsername().equals("")) {
 					pStmt.setString(1, users.getUsername());
 				}
 				else {
-					pStmt.setString(1, users.getOld_username());
+					pStmt.setString(1, "");
 				}
 				if (users.getPassword() != null && !users.getPassword().equals("")) {
 					pStmt.setString(2, users.getPassword());
 				}
 				else {
-					pStmt.setString(2, users.getOld_pw());
+					pStmt.setString(2, "");
 				}
 				if (users.getMail() != null && !users.getMail().equals("")) {
 					pStmt.setString(3, users.getMail());
 				}
 				else {
-					pStmt.setString(3, users.getOld_mail());
+					pStmt.setString(3, "");
 				}
 				if (users.getOld_username() != null && !users.getOld_username().equals("")) {
 					pStmt.setString(4, users.getOld_username());
@@ -268,33 +279,24 @@ public class UsersDAO  {
 					}
 				}
 			}
-
 			// 結果を返す
 			return result;
 		}
-
 		public String getUser_id(String username) {
         	Connection conn = null;
     		String result = null;
-
     		try {
     			// JDBCドライバを読み込む
     			Class.forName("org.h2.Driver");
-
     			// データベースに接続する
     			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
     			// SQL文を準備する
     			String sql = "SELECT user_id from Users WHERE username = ?";
     			PreparedStatement pStmt = conn.prepareStatement(sql);
-
     			// SQL文を完成させる
 				pStmt.setString(1, username);
-
-
     			// SQL文を実行し、結果表を取得する
     			ResultSet rs = pStmt.executeQuery();
-
     			// 結果表をコレクションにコピーする
     			while (rs.next()) {
     				result = rs.getString("user_id");
@@ -320,7 +322,6 @@ public class UsersDAO  {
     				}
     			}
     		}
-
     		// 結果を返す
     		return result;
         }
