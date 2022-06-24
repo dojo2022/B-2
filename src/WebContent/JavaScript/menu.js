fb4da4ba96bcaca1326e30c70ab8316c071dcad6
dojo2022-Takeda <103238150+dojo2022-Takeda@users.noreply.github.com>
2022-06-24 14:44:38 +0900

    メニュー関連（css未完成)

--- src/WebContent/JavaScript/menu.js
+++ src/WebContent/JavaScript/menu.js
@@ -1,3 +1,5 @@
+
+
 const weeks = ['日', '月', '火', '水', '木', '金', '土']
 const date = new Date()
 let year = date.getFullYear()
@@ -8,116 +10,117 @@ const count = document.getElementById("count").value
 var cArray = new Array(count);
 var tArray = new Array(count);
 
-for(let i = 0; i < count; i++){
-	cArray[i] = document.getElementById("cer"+i).value
-	tArray[i] = document.getElementById("test"+i).value
+for (let i = 0; i < count; i++) {
+	cArray[i] = document.getElementById("cer" + i).value
+	tArray[i] = document.getElementById("test" + i).value
 }
 
-for(let i = 0; i < count; i++){
+for (let i = 0; i < count; i++) {
 	console.log(cArray[i])
 	console.log(tArray[i])
 }
 
 
 function showCalendar(year, month) {
-    const calendarHtml = createCalendar(year, month)
-    const sec = document.createElement('section')
-    sec.innerHTML = calendarHtml
-    document.querySelector('#calendar').appendChild(sec)
+	const calendarHtml = createCalendar(year, month)
+	const sec = document.createElement('section')
+	sec.innerHTML = calendarHtml
+	document.querySelector('#calendar').appendChild(sec)
 }
 
 function createCalendar(year, month) {
-    const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
-    const endDate = new Date(year, month,  0) // 月の最後の日を取得
-    const endDayCount = endDate.getDate() // 月の末日
-    const lastMonthEndDate = new Date(year, month - 2, 0) // 前月の最後の日の情報
-    const lastMonthendDayCount = lastMonthEndDate.getDate() // 前月の末日
-    const startDay = startDate.getDay() // 月の最初の日の曜日を取得
-    let dayCount = 1 // 日にちのカウント
-    let calendarHtml = '' // HTMLを組み立てる変数
+	const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
+	const endDate = new Date(year, month, 0) // 月の最後の日を取得
+	const endDayCount = endDate.getDate() // 月の末日
+	const lastMonthEndDate = new Date(year, month - 2, 0) // 前月の最後の日の情報
+	const lastMonthendDayCount = lastMonthEndDate.getDate() // 前月の末日
+	const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+	let dayCount = 1 // 日にちのカウント
+	let calendarHtml = '' // HTMLを組み立てる変数
 
-    calendarHtml += '<h1>' + year  + '/' + month + '</h1>'
-    calendarHtml += '<table class="table_calendar">'
+	calendarHtml += '<h1>' + year + '/' + month + '</h1>'
+	calendarHtml += '<table class="table_calendar">'
 
-    // 曜日の行を作成
-    for (let i = 0; i < weeks.length; i++) {
-        calendarHtml += '<td>' + weeks[i] + '</td>'
-    }
+	// 曜日の行を作成
+	for (let i = 0; i < weeks.length; i++) {
+		calendarHtml += '<td>' + weeks[i] + '</td>'
+	}
 
-    for (let w = 0; w < 6; w++) {
-        calendarHtml += '<tr>'
+	for (let w = 0; w < 6; w++) {
+		calendarHtml += '<tr>'
 
-        loop: for (let d = 0; d < 7; d++) {
-            if (w == 0 && d < startDay) {
-                // 1行目で1日の曜日の前
-                let num = lastMonthendDayCount - startDay + d + 1
-                calendarHtml += '<td class="is-disabled">' + num + '</td>'
-            } else if (dayCount > endDayCount) {
-                // 末尾の日数を超えた
-                let num = dayCount - endDayCount
-                calendarHtml += '<td class="is-disabled">' + num + '</td>'
-                dayCount++
-            } else {
-            	const daytext = year + '/' + month + '/' + dayCount
-    	        for(let i = 0; i < count; i++){
-        			if(tArray[i] === daytext){
-        				console.log('test')
-        				calendarHtml += `<td class="calendar_td" id="check" data-date="${year}/${month}/${dayCount}">${dayCount}</td>`
+		loop: for (let d = 0; d < 7; d++) {
+			if (w == 0 && d < startDay) {
+				// 1行目で1日の曜日の前
+				let num = lastMonthendDayCount - startDay + d + 1
+				calendarHtml += '<td class="is-disabled">' + num + '</td>'
+			} else if (dayCount > endDayCount) {
+				// 末尾の日数を超えた
+				let num = dayCount - endDayCount
+				calendarHtml += '<td class="is-disabled">' + num + '</td>'
+				dayCount++
+			} else {
+				const daytext = year + '/' + month + '/' + dayCount
+				for (let i = 0; i < count; i++) {
+					if (tArray[i] === daytext) {
+						console.log('test')
+						calendarHtml += `<td class="calendar_td" id="check" data-date="${year}/${month}/${dayCount}">${dayCount}</td>`
 						dayCount++;
-        				continue loop;
-        			}
-        		}
-                calendarHtml += `<td class="calendar_td" data-date="${year}/${month}/${dayCount}">${dayCount}</td>`
-                dayCount++
-            }
-        }
-        calendarHtml += '</tr>'
-    }
-    calendarHtml += '</table>'
+						continue loop;
+					}
+				}
+				calendarHtml += `<td class="calendar_td" data-date="${year}/${month}/${dayCount}">${dayCount}</td>`
+				dayCount++
+			}
+		}
+		calendarHtml += '</tr>'
+	}
+	calendarHtml += '</table>'
 
-    return calendarHtml
+	return calendarHtml
 }
 
 function moveCalendar(e) {
-    document.querySelector('#calendar').innerHTML = ''
+	document.querySelector('#calendar').innerHTML = ''
 
-    if (e.target.id === 'prev') {
-        month--
+	if (e.target.id === 'prev') {
+		month--
 
-        if (month < 1) {
-            year--
-            month = 12
-        }
-    }
+		if (month < 1) {
+			year--
+			month = 12
+		}
+	}
 
-    if (e.target.id === 'next') {
-        month++
+	if (e.target.id === 'next') {
+		month++
 
-        if (month > 12) {
-            year++
-            month = 1
-        }
-    }
+		if (month > 12) {
+			year++
+			month = 1
+		}
+	}
 
-    showCalendar(year, month)
+	showCalendar(year, month)
 }
 
 document.querySelector('#prev').addEventListener('click', moveCalendar)
 document.querySelector('#next').addEventListener('click', moveCalendar)
 
 document.addEventListener("click", function(e) {
-    if(e.target.classList.contains("calendar_td")) {
-        for(let i = 0; i < count; i++){
-        	if(e.target.dataset.date === tArray[i]){
-        		alert(cArray[i] + 'の試験日です！')
-        	}
-        }
-    }
+	if (e.target.classList.contains("calendar_td")) {
+		for (let i = 0; i < count; i++) {
+			if (e.target.dataset.date === tArray[i]) {
+				alert(cArray[i] + 'の試験日です！')
+			}
+		}
+	}
 })
 
-
-document.querySelector('.menu-btn').addEventListener('click', function(){
-    document.querySelector('.menu').classList.toggle('is-active');
+// ハンバーガーメニューの動作
+document.querySelector('.menu-btn').addEventListener('click', function() {
+	document.querySelector('.menu').classList.toggle('is-active');
 });
+
 
 showCalendar(year, month)
--- src/WebContent/WEB-INF/jsp/menu.jsp
+++ src/WebContent/WEB-INF/jsp/menu.jsp
@@ -1,37 +1,43 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="/tasuma/css/menu.css">
 <link rel="stylesheet" href="/tasuma/css/style.css">
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>TASUMA|メニュー</title>
 <script defer src="JavaScript/menu.js"></script>
 </head>
 <body>
-<% int count = 0; %>
+	<%
+		int count = 0;
+	%>
 	<c:forEach var="e" items="${menu_data }">
 		<input type="hidden" id="cer<%= count %>" value="${e.certification}">
 		<input type="hidden" id="test<%= count %>" value="${e.testDay }">
-		<% count++; %>
+		<%
+			count++;
+		%>
 	</c:forEach>
-	<input type="hidden" id="count" value="<%= count %>">
-<!-- ヘッダーここから -->
+	<input type="hidden" id="count" value="<%=count%>">
+	<!-- ヘッダーここから -->
 	<jsp:include page="/WEB-INF/jsp/header.jsp" />
-<!-- ヘッダーここまで -->
+	<!-- ヘッダーここまで -->
 
-<!-- ユーザ名の表示 -->
+	<!-- ユーザ名の表示 -->
 	<p class="username">ユーザ名:${username.username }</p>
 	<h2>メニュー</h2>
 
-<!-- ハンバーガーメニュー -->
-<!-- 各リンクの内、サーブレットが未作成のものはとりあえずMenuServletを指定 -->
+	<!-- ハンバーガーメニュー -->
+	<!-- 各リンクの内、サーブレットが未作成のものはとりあえずMenuServletを指定 -->
 
-<!-- ハンバーガーメニューの元の部分 -->
+	<!-- ハンバーガーメニューの元の部分 -->
+	<!--
 <nav class="menu" id="js-nav">
 		<ul class="nav__items nav-items">
 			<li class="nav-items__item"><a href="/tasuma/CertificationServlet">My資格</a></li>
@@ -47,49 +53,52 @@
 		<span></span>
 		<span></span>
 	</button>
+	-->
 
-<!-- 竹田が追記 ※未完成 -->
-<!--
+
+	<!-- ハンバーガーメニュー追記ここから  竹田 -->
+<body>
 	<button type="button" class="menu-btn">
 		<i class="fa fa-bars" aria-hidden="true"></i>
 	</button>
-
 	<div class="menu">
-			<div class="menu-item"><a href="/tasuma/CertificationServlet">My資格</a></div>
-			<div class="menu-item"><a href="/tasuma/TargetServlet">目標設定</a></div>
-			<div class="menu-item"><a href="/tasuma/UnderstandServlet">理解度報告</a></div>
-			<div class="menu-item"><a href="/tasuma/BbsServlet">掲示板</a></div>
-			<div class="menu-item"><a href="/tasuma/UserSettingServlet">ユーザ設定</a></div>
-			<div class="menu-item"><a href="/tasuma/LoginServlet">ログアウト</a></div>
-		</div>
-		-->
-<!-- 追記ここまで -->
+		<div class="menu__item"><a href="/tasuma/CertificationServlet">My資格</a></div>
+		<div class="menu__item"><a href="/tasuma/TargetServlet">目標設定</a></div>
+		<div class="menu__item"><a href="/tasuma/UnderstandServlet">理解度報告</a></div>
+		<div class="menu__item"><a href="/tasuma/BbsServlet">掲示板</a></div>
+		<div class="menu__item"><a href="/tasuma/UserSettingServlet">ユーザ設定</a></div>
+		<div class="menu__item"><a href="/tasuma/LoginServlet">ログアウト</a></div>
+	</div>
 
-<!-- 本日の目標 -->
+	<!-- ハンバーガーメニューの追記ここまで -->
+
+	<!-- 本日の目標 -->
 	<c:forEach var="e1" items="${menu_data }">
 		<h3>本日の目標-${e1.certification}</h3>
-		<ul>
+		<ul class="data">
 			<c:forEach var="e2" items="${e1.itemList }">
 				<li>${e2 }
 			</c:forEach>
 		</ul>
 	</c:forEach>
 
-<!-- 残り日数 -->
+	<!-- 残り日数 -->
 	<c:forEach var="e" items="${menu_data }">
 		<h4>${e.certification}まで残り${e.remainingDays }日です。</h4>
 	</c:forEach>
 
 
-<!-- カレンダー -->
+	<!-- カレンダー -->
 	<!-- 予定がある場合印付ける→カーソル合わせると詳細表示(余裕があれば) -->
 
-	<button id="prev" type="button">前の月</button>
-	<button id="next" type="button">次の月</button>
-	<div id="calendar"></div>
+	<div class="calenderelese">
+		<button id="prev" type="button">前の月</button>
+		<button id="next" type="button">次の月</button>
+		<div id="calendar"></div>
+	</div>
 
-<!-- フッターここから -->
+	<!-- フッターここから -->
 	<jsp:include page="/WEB-INF/jsp/footer.jsp" />
-<!-- フッターここまで -->
+	<!-- フッターここまで -->
 </body>
 </html>
